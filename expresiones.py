e3be053acf462528c2413ded64059400ccc3bea0
Vázquez <m.a.vazquez@accenture.com>
2016-07-08 11:58:44 -0700

    Añadí archivo para la práctica de expresiones.

--- expresiones.py
+++ expresiones.py
@@ -1,10 +1,18 @@
 #Ejemplo de If else
+#Operador de concatenacion '+'
 a= 1
-b=2
+b=2.2
 
 if a > b:
     print ("Estamos en buenas ondas.")
 elif b < a:
-    print ("En que ondas estamos?>>>>>>~~~~~~~2121")
+
+    print ("En que ondas estamos?>>>>>>~~~~~~~2121" +  b)
 else:
     print ("En que ondas estamos?>>>>>>~~~~~~~")
+
+#Manera de hacer conversiones a formato String para que puedan ser impresas
+# %s == String , %d == Decimal , %f == Float
+#Ejemplo de Uso:
+
+print ("Valores que se deben desplegar " + %f + " " + %d (a,b))
