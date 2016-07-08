ca5d2f15bbde7ef1c771e3050efa8b3ce66c834f
Vázquez <m.a.vazquez@accenture.com>
2016-07-08 11:51:51 -0700

    C

---
+++ expresiones.py
@@ -0,0 +1,10 @@
+#Ejemplo de If else
+a= 1
+b=2
+
+if a > b:
+    print ("Estamos en buenas ondas.")
+elif b < a:
+    print ("En que ondas estamos?>>>>>>~~~~~~~2121")
+else:
+    print ("En que ondas estamos?>>>>>>~~~~~~~")
